6b101138e5f6d980201abe60ed85ac29beb72167
Zak <zakary.howard94@gmail.com>
2018-09-09 12:11:08 +0200

    if N%15 return FizzBuzz working with full function, start feature testing

--- features/index.feature.js
+++ features/index.feature.js
@@ -2,7 +2,7 @@ const { FizzBuzz } = require('../spec/spec.helper');
 const BrowserHelpers = require('e2e_training_wheels');
 const browser = new BrowserHelpers();
  
-describe('sample UI test', () => {
+describe('User can input a value and get FizzBuzz results', () => {
   before( async () => {
     await browser.init();
     await browser.visitPage('http://localhost:8080/');
@@ -16,7 +16,10 @@ describe('sample UI test', () => {
     await browser.close();
   });
  
-  it('/* description inserted here */', async () => {
-    //  Write tour scenario  
-  });
+  it('clicking on the "Check" button', async () => {
+      await browser.fillIn("input[id='value']", { with: "3"})
+      await browser.clickOnButton("input[value='Check']")
+      let content = await browser.getContent("[id='display_answer']")
+      expect(content).to.eql('Fizz');
+  })
 });
