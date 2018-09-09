493193f7b0c711bd2e819a21458c941ffcb4b5aa
Zak <zakary.howard94@gmail.com>
2018-09-09 09:54:34 +0200

    fixed typos, if N%3 then return 'Fizz' test passing

---
+++ features/index.feature.js
@@ -0,0 +1,22 @@
+const { FizzBuzz } = require('../spec/spec.helper');
+const BrowserHelpers = require('e2e_training_wheels');
+const browser = new BrowserHelpers();
+ 
+describe('sample UI test', () => {
+  before( async () => {
+    await browser.init();
+    await browser.visitPage('http://localhost:8080/');
+  });
+ 
+  beforeEach(async () => {
+    await browser.page.reload();
+  });
+ 
+  after(async () => {
+    await browser.close();
+  });
+ 
+  it('/* description inserted here */', async () => {
+    //  Write tour scenario  
+  });
+});
